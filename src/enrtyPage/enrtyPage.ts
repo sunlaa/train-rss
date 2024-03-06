801f905b5533bb4d3f4b9e4c8d55e98f49c9d785
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-06 18:35:40 +0100

    add page

---
+++ src/enrtyPage/enrtyPage.ts
@@ -0,0 +1,8 @@
+// import { BaseElement } from '../components/base-element';
+// import './entryPage.css';
+
+// class EntryPage extends BaseElement {
+//   constructor() {
+//     super({ tag: 'section', className: 'entryPage' });
+//   }
+// }
